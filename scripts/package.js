e773dffa28b50a2213da7ee766901b8d66e97d77
Scott Thiessen <things@sccottt.com>
2018-02-05 16:02:27 -0800

    Add package.json configuration

--- scripts/package.js
+++ scripts/package.js
@@ -1,53 +1,67 @@
 
-const fse          = require('fs-extra'),
-      logBox       = require('log-box'),
-      parseArgs    = require('minimist'),
-      { execSync } = require('child_process'),
-      pkg          = require('../package.json');
+const PKG_CONF_NAMESPACE = 'packageApp';
 
-const args = parseArgs(process.argv.slice(2), {
+const fse            = require('fs-extra'),
+      logBox         = require('log-box'),
+      parseArgs      = require('minimist'),
+      { execSync }   = require('child_process'),
+      pkg            = require('../package.json');
+
+const args           = parseArgs(process.argv.slice(2), {
   alias: {
-    w: 'windows',
-    z: 'zip'
+    w: 'windows'
   }
 });
 
-const isWindows      = !!args.windows,
-      doZip          = !!args.zip;
+const pkgConf        = pkg[PKG_CONF_NAMESPACE] || {},
+      toWindows      = !!args.windows;
 
 const appId          = pkg.name,
-      appTitle       = pkg.productName || pkg.name,
-      appArch        = 'x64',
-      appPlatform    = isWindows ? 'win32' : 'darwin',
-      platformName   = isWindows ? 'Windows (via Wine)' : 'macOS',
-      outputFolder   = args.output || 'packages',
+      appTitle       = pkg.productName || pkg.name;
+
+const appPlatform    = toWindows ? 'win32' : 'darwin',
+      platformName   = toWindows ? 'Windows (via Wine)' : 'macOS',
+      appArch        = pkgConf.arch || 'x64';
+
+const outputFolder   = pkgConf.outputFolder || 'packages',
       outputFilename = `${appTitle}-${appPlatform}-${appArch}`,
-      date           = new Date(),
+      winePath       = pkgConf.winePath || '/Applications/Wine Stable.app';
+
+const date           = new Date(),
       dateString     = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2),
-      zipFilename    = `${dateString}-${appId}-mac.zip`,
-      pathToWine     = `/Applications/Wine Stable.app`;
+      zipIt          = !!pkgConf.zip,
+      zipFilename    = `${dateString}-${appId}-mac.zip`;
+
+
+const makeCmd = () => {
+
+  let cmd = '';
+
+  cmd += `webpack --config config/webpack.prod.js;`
+      + `mkdir -p ${outputFolder};`;
+
+  if (toWindows) {
+    cmd += `test "$?BASH_VERSION" = "0" || eval 'setenv() { export "$1=$2"; }';`
+        + `setenv PATH "${winePath}/Contents/Resources/start/bin:${winePath}/Contents/Resources/wine/bin:$PATH";`;
+  }
+
+  cmd += `electron-packager . '${appTitle}' --out=${outputFolder} --overwrite --platform=${appPlatform} --arch=${appArch};`;
+
+  if (zipIt) {
+    cmd += `cd ${outputFolder};`
+        + `zip -r '${zipFilename}' '${outputFilename}';`
+        + `rm -rf '${outputFilename}';`
+        + `cd ..;`;
+  }
+
+  return cmd;
+
+}
 
 logBox(`Packaging ${appTitle} for ${platformName}`);
 
 fse.removeSync(`app/build/*`);
 
-let cmd = `webpack --config config/webpack.prod.js;`
-        + `mkdir -p ${outputFolder};`;
+execSync(makeCmd(), { stdio: 'inherit' });
 
-if (isWindows) {
-  cmd += `test "$?BASH_VERSION" = "0" || eval 'setenv() { export "$1=$2"; }';`
-      + `setenv PATH "${pathToWine}/Contents/Resources/start/bin:${pathToWine}/Contents/Resources/wine/bin:$PATH";`;
-}
-
-cmd += `electron-packager . '${appTitle}' --out=${outputFolder} --overwrite --platform=${appPlatform} --arch=${appArch};`;
-
-if (doZip) {
-  cmd += `cd ${outputFolder};`
-      + `zip -r '${zipFilename}' '${outputFilename}';`
-      + `rm -rf '${outputFilename}';`
-      + `cd ..;`;
-}
-
-execSync(cmd, { stdio: 'inherit' });
-
-logBox(`Package ready: ${outputFolder}/${doZip ? zipFilename : outputFilename}`);
+logBox(`Package ready: ${outputFolder}/${zipIt ? zipFilename : outputFilename}`);
